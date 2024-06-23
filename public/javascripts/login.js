8f7dbe9e6b1fed170860ed0b3ddfccc6e673b058
Amet <162996755+Ameteh@users.noreply.github.com>
2024-06-23 17:06:54 +0700

    Delete public directory

--- public/javascripts/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('showRegisterForm').addEventListener('click', function() {
-    document.getElementById('loginForm').classList.remove('active');
-    document.getElementById('registerForm').classList.add('active');
-});
-
-document.getElementById('showLoginForm').addEventListener('click', function() {
-    document.getElementById('registerForm').classList.remove('active');
-    document.getElementById('loginForm').classList.add('active');
-});
